71a504b0a91a0bad9ab5e16475f5be24d7d1778c
DL Networks <dlnetworks@users.noreply.github.com>
2017-08-27 12:09:02 -0700

    Update shoutcast.php

--- shoutcast.php
+++ shoutcast.php
@@ -22,14 +22,23 @@ $timeout = "5";
 # refresh time in seconds (0 to disable)
 $refresh = "60";
 
-# server ip or hostname (without http://)
-$ip = "127.0.0.1";
-
+# server ip/hostname (without http://)
 # server port
-$port = "80";
+# stream id (sc V1 use sid 1)
+# song from first server used for tweet
+# add or remove servers as needed
 
-# stream id. for V1 use sid 1
-$sid = "1";
+$ip[0] = "127.0.0.1";
+$port[0] = "8000";
+$sid[0] = "1";
+
+$ip[1] = "127.0.0.1";
+$port[1] = "8000";
+$sid[1] = "2";
+
+$ip[2] = "127.0.0.1";
+$port[2] = "8000";
+$sid[2] = "3";
 
 # url to include at the end of the tweet
 $url = "https://www.domain.com";
@@ -37,27 +46,41 @@ $url = "https://www.domain.com";
 # text to iclude at the beginning of the tweet
 $prefix = "#hastag";
 
-//END CONFIGURATION
+// END CONFIGURATION
 
-if ($refresh != "0") 
-	{
-	print "<html><head><meta http-equiv=\"refresh\" content=\"$refresh\"></head><body>\n";
-	}
 $twitterObj = new EpiTwitter($consumer_key, $consumer_secret, $token, $secret);
 $twitterObjUnAuth = new EpiTwitter($consumer_key, $consumer_secret);
-$fp = @fsockopen($ip,$port,$errno,$errstr,$timeout);
-if ($fp) {
-	fputs($fp, "GET /7.html?sid=$sid HTTP/1.0\r\nUser-Agent: Mozilla/5.0 (The King Kong of Lawn Care)\r\n\r\n");
-	while (!feof($fp)) {
-		$info = fgets($fp);
+
+$i = "0";
+$servers = count($ip);
+while($i<=$servers)	{
+	$fp = @fsockopen($ip[$i],$port[$i],$errno,$errstr,$timeout);
+	if ($fp) {
+		fputs($fp, "GET /7.html?sid=$sid[$i] HTTP/1.0\r\nUser-Agent: Mozilla/5.0 (The King Kong of Lawn Care)\r\n\r\n");
+		while (!feof($fp)) {
+			$info = fgets($fp);
+			};
+		$info = str_replace('<html><body>', "", $info);
+		$info = str_replace('</body></html>', "", $info);
+		$stats = explode(',', $info);
+		$track[$i] = $stats[6];
+		$listeners[$i] = $stats[0];
 		};
-	$info = str_replace('<html><body>', "", $info);
-	$info = str_replace('</body></html>', "", $info);
-	$stats = explode(',', $info);
-	$song = $stats[6];
+	$i++;
 };
 
-$tweet = "$prefix: $song - $url";
+$i = "0";
+$song = $track[0];
+$total_listeners = array_sum($listeners);
+
+if ($count != "0") 
+	{
+	$tweet = "$prefix $song - $total_listeners Locked - $url";
+	}
+else
+	{
+	$tweet = "$prefix $song - $url";
+	}
 
 if ($refresh != "0") 
 	{
